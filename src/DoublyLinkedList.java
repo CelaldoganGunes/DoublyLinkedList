7f6527a14f3e8823e5f61f09bba5aedb282a8140
Celaldoğan Güneş <celaldogan2002@outlook.com>
2022-05-23 15:13:37 +0300

    Update DoublyLinkedList.java

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -3,6 +3,22 @@ public class DoublyLinkedList {
     private Node head;
     private Node tail;
 
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
     public class Node
     {
         private Node previousNode;
@@ -52,6 +68,24 @@ public class DoublyLinkedList {
     }
 
 
+    public DoublyLinkedList()
+    {
+        this.setHead(null);
+        this.setTail(null);
+    }
 
+    public void insertToList(Student student)
+    {
+
+        //Liste boş ise ekleme kısmı
+        if (this.getHead() == null) {
+            Node newNode = new Node(null,null,student);
+            this.setHead(newNode);
+            this.setTail(newNode);
+            return;
+        }
+
+        return;
+    }
 
 }
